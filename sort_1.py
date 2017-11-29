36a1b409e2f169156b4d7bf485598a172c5e9d56
yaly <yaly.zhao@gmail.com>
2017-11-29 10:15:59 +0800

    version 1

--- sort_1.py
+++ sort_1.py
@@ -1,14 +1,12 @@
 #coding:utf-8
-"""Sort test
-"""
+"""Sort test"""
 
 NUM_LIST = [
-    [1, 9, 8, 5, 6, 7, 4, 3, 2],
     [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 9, 8]
+    [1, 2, 7, 4, 5, 3, 6, 9, 8]
 ]
 
-NUMS = NUM_LIST[2]
+NUMS = NUM_LIST[1]
 print NUMS
 LENGTH = len(NUMS)
 
@@ -17,15 +15,10 @@ COUNT = 0
 
 # bubble_sort
 for i  in range(LENGTH):
-    FLAG = False
     for j in range(LENGTH-i-1):
         COUNT += 1
-        if NUMS[j] > NUMS[j+1]:
-            TMP = NUMS[j]
-            NUMS[j] = NUMS[j+1]
-            NUMS[j+1] = TMP
+        if NUMS[j] > NUMS[j + 1]:
+            NUMS[j], NUMS[j + 1] = NUMS[j+1], NUMS[j]
             COUNT_SWAP += 1
-    if not FLAG:
-        break
 
 print NUMS, COUNT_SWAP, COUNT
